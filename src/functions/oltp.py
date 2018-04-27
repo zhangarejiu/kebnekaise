fc51fa5a91da72a6322b36b248ff5203bccd6a8c
kebnekaise-io <developer@kebnekaise.io>
2018-04-27 19:38:13 -0300

    v1524868674

--- src/functions/oltp.py
+++ src/functions/oltp.py
@@ -193,7 +193,7 @@ class Trader(object):
             price = (1 + margin / 100) * l_ask
             amount = self.Toolkit.Quota / price
 
-            if self.Toolkit.Quota < balance[quote][0] <= 2 * self.Toolkit.Quota:
+            if balance[quote][0] <= 2 * self.Toolkit.Quota:
                 amount = balance[quote][0] / price
             params = {'amount': round(amount, 8), 'price': round(price, 8), 'symbol': symbol, }
 
