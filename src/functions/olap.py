98888b654c81251c505efc334d7ed47dfcee41db
kebnekaise-io <developer@kebnekaise.io>
2018-04-28 01:49:22 -0300

    v1524890953

--- src/functions/olap.py
+++ src/functions/olap.py
@@ -20,7 +20,7 @@ class Indicator(object):
         self.log = self.Toolkit.log
         self.log(self.Toolkit.Greeting, self)
 
-    def broadway(self, bsize=5):
+    def broadway(self):
         """
         https://www.pokernews.com/pokerterms/broadway.htm
         """
@@ -34,10 +34,10 @@ class Indicator(object):
             bw = {s: round(i, 3) for s, i in self._cache.items() if i > 0}
             self.log('Financial indexes are: ' + str(bw), self)
 
-            bw = dict(sorted(bw.items(), key=lambda k: k[1])[-bsize:])
+            bw = dict(sorted(bw.items(), key=lambda k: k[1])[-5:])
             self.log('Preliminary selection is: ' + str(bw), self)
 
-            bw = [bw, {}][sum(bw.values()) / bsize < 7 or len(bw) < 4]
+            bw = [{}, bw][sum(bw.values()) / 5 > 5]
             self.log('Final selection is: ' + str(bw), self)
             return bw
         except:
@@ -59,16 +59,9 @@ class Indicator(object):
             t_delta = time.time()
 
             for s in symbols:
-                if self.Toolkit.halt():
-                    break
-
-                t = self._trend(s)
-                if t is not None and t < 0:
-                    m = self._momentum(s)
-                    if m is not None and m > 0:
-                        i = self.Toolkit.smooth(-t * m)
-                        if i is not None:
-                            self._cache[s] = (self._cache[s] + i) / 2
+                I = self._index(s)
+                if I is not None:
+                    self._cache[s] = (self._cache[s] + I) / 2
 
             t_delta = time.time() - t_delta
             av_delta = t_delta / ls
@@ -77,42 +70,40 @@ class Indicator(object):
         except:
             self.log(traceback.format_exc(), self)
 
-    def _trend(self, symbol):
+    def _index(self, symbol):
         """
         """
 
         try:
+            if self.Toolkit.halt():
+                return
+
             t24h = self.Wrapper.ticker24h(symbol)
-            x, y, z = 0., 0., 0.
+            assert t24h is not None
 
-            if t24h is not None:
-                _open, _high, _low, _close, _volume = t24h
-                if _volume > 100 and _close > 1E-5:
-                    x = 100 * (_close / _high - 1)  # always <= 0
-                    y = 100 * (_close / _low - 1)  # always >= 0
-                    z = 100 * (_close / _open - 1)  # any
-
-            return .25 * x + .25 * y + .5 * z
-        except:
-            self.log(traceback.format_exc(), self)
-
-    def _momentum(self, symbol):
-        """
-        """
-
-        try:
             book = self.Wrapper.book(symbol, 5)
-            asks, bids = 0., 0.
+            assert book is not None
 
-            if book is not None:
-                for price, amount in book.items():
-                    if amount > 0:
-                        asks += price * amount
-                    else:
-                        bids += price * amount
+            _open, _high, _low, _close, _volume = t24h
+            assert _volume > 100 and _close > 1E-5
 
+            variation = 100 * (_close / _open - 1)
+            assert abs(variation) < 5
+
+            volatility = 100 * (_high / _low - 1)
+            assert volatility < 5
+
+            asks, bids, trend = 0., 0., 0.
+            for price, amount in book.items():
+                if amount > 0:
+                    asks += price * amount
+                else:
+                    bids += price * amount
             if asks > 0:
-                return 100 * (abs(bids / asks) - 1)
-            return asks
+                trend = 100 * (abs(bids / asks) - 1)
+            return self.Toolkit.smooth(trend)
+
+        except AssertionError:
+            return 0.
         except:
             self.log(traceback.format_exc(), self)
