df29705c607bd4361184acdf47b025a685639168
kebnekaise-io <developer@kebnekaise.io>
2018-06-06 06:57:34 -0300

    v1528279044

--- bin/.drafts/profits.py
+++ bin/.drafts/profits.py
@@ -1,21 +1,36 @@
 import os
+import time
 import traceback
 
 
-separator = '$$$$$ $$$$$ $$$$$ $$$$$ $$$$$ $$$$$ $$$$$'
-path = '../../logs'
+now_str = time.strftime('%Y.%m.%d.%Z.%H.%M.%S', time.gmtime())
+now_str = now_str.replace('GMT', 'UTC')
+today = now_str[:10].replace('.', '_')
+cache = {}
+
+
+def _print(msg):
+    with open(today + '.txt', 'w') as fp:
+        fp.write(str(msg))
+
 
 try:
-    cache = {}
+    path = '../../logs'
+    assert os.path.exists(path)
+
     for logfile in os.listdir(path):
         with open(path + '/' + logfile, 'r') as fp:
             cache[logfile] = []
             for line in fp.readlines():
                 if 'profits' in line:
                     d = eval(line[52:])
-                    cache[logfile].append(
-                        (line[:23], {k: v for k, v in d['profits'].items() if k in d['protected'][:2]})
-                    )
-    print(cache)
+                    cache[logfile].append((line[:23], {
+                        k: v for k, v in d['profits'].items()
+                        if k in d['protected'][:2]
+                    }))
+    _print(cache)
+
+except AssertionError:
+    _print(cache)
 except:
-    print(traceback.format_exc())
+    _print(traceback.format_exc())
